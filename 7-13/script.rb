dc9c1553e1339e97fb8652b5e3426dcbff1b1676
mcbrooks95 <mcbrooks95@gmail.com>
2016-07-13 19:36:00 -0400

    playing around in ruby

--- 7-13/script.rb
+++ 7-13/script.rb
@@ -5,3 +5,23 @@ print "\n"
 
 print "123456's length is "
 puts "123456".length
+
+puts "12345".reverse
+
+puts "HeY wAAZup".upcase
+
+puts "EdFuJ".downcase
+
+#one line comment
+
+=begin
+asdf
+asdf
+asdf
+MULTILINE
+COMMENTS
+RULE
+=end
+puts "multiline comment is over"
+
+puts "aDd".upcase.downcase.reverse
